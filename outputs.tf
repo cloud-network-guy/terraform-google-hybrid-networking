b845b3152321968d63f6bb892cfaaa3eaf4a9a19
cloud-network-guy <greenlakejohnny@gmail.com>
2024-01-02 17:51:54 -0800

    ipsec ranges

--- outputs.tf
+++ outputs.tf
@@ -31,7 +31,7 @@ output "vpn_tunnels" {
       cloud_router_ip_address = v.ip_range
       peer_ip_address         = v.peer_ip_address
       peer_gateway_ip         = try(google_compute_vpn_tunnel.default[v.index_key].peer_ip, null)
-      cloud_vpn_gateway_ip    = try(data.google_compute_ha_vpn_gateway.default[v.cloud_vpn_gateway_key].vpn_interfaces.*.ip_address[v.tunnel_index], "unknown")
+      cloud_vpn_gateway_ip    = try(data.google_compute_ha_vpn_gateway.default[v.cloud_vpn_gateway_key].vpn_interfaces.*.ip_address[v.tunnel_index % 2 == 0 ? 0 : 1], "unknown")
       ike_version             = v.ike_version
       shared_secret           = v.shared_secret
       detailed_status         = try(google_compute_vpn_tunnel.default[v.index_key].detailed_status, "Unknown")
